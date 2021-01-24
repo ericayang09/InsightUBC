b23a2d806c4fdedf944fb9ce900256b6ad6100c2
classy <classy@cs.ubc.ca>
2021-01-24 23:09:26 +0000

    Starter files

---
+++ src/controller/InsightFacade.ts
@@ -0,0 +1,31 @@
+import Log from "../Util";
+import {IInsightFacade, InsightDataset, InsightDatasetKind} from "./IInsightFacade";
+import {InsightError, NotFoundError} from "./IInsightFacade";
+
+/**
+ * This is the main programmatic entry point for the project.
+ * Method documentation is in IInsightFacade
+ *
+ */
+export default class InsightFacade implements IInsightFacade {
+
+    constructor() {
+        Log.trace("InsightFacadeImpl::init()");
+    }
+
+    public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
+        return Promise.reject("Not implemented.");
+    }
+
+    public removeDataset(id: string): Promise<string> {
+        return Promise.reject("Not implemented.");
+    }
+
+    public performQuery(query: any): Promise<any[]> {
+        return Promise.reject("Not implemented.");
+    }
+
+    public listDatasets(): Promise<InsightDataset[]> {
+        return Promise.reject("Not implemented.");
+    }
+}
---
+++ test/InsightFacade.spec.ts
@@ -0,0 +1,146 @@
+import { expect } from "chai";
+import * as chai from "chai";
+import * as fs from "fs-extra";
+import * as chaiAsPromised from "chai-as-promised";
+import {InsightDatasetKind} from "../src/controller/IInsightFacade";
+import InsightFacade from "../src/controller/InsightFacade";
+import Log from "../src/Util";
+import TestUtil from "./TestUtil";
+
+// This extends chai with assertions that natively support Promises
+chai.use(chaiAsPromised);
+
+// This should match the schema given to TestUtil.validate(..) in TestUtil.readTestQueries(..)
+// except 'filename' which is injected when the file is read.
+export interface ITestQuery {
+    title: string;
+    query: any;  // make any to allow testing structurally invalid queries
+    isQueryValid: boolean;
+    result: any;
+    filename: string;  // This is injected when reading the file
+}
+
+describe("InsightFacade Add/Remove/List Dataset", function () {
+    // Reference any datasets you've added to test/data here and they will
+    // automatically be loaded in the 'before' hook.
+    const datasetsToLoad: { [id: string]: string } = {
+        courses: "./test/data/courses.zip",
+    };
+    let datasets: { [id: string]: string } = {};
+    let insightFacade: InsightFacade;
+    const cacheDir = __dirname + "/../data";
+
+    before(function () {
+        // This section runs once and loads all datasets specified in the datasetsToLoad object
+        // into the datasets object
+        Log.test(`Before all`);
+        if (!fs.existsSync(cacheDir)) {
+            fs.mkdirSync(cacheDir);
+        }
+        for (const id of Object.keys(datasetsToLoad)) {
+            datasets[id] = fs.readFileSync(datasetsToLoad[id]).toString("base64");
+        }
+        try {
+            insightFacade = new InsightFacade();
+        } catch (err) {
+            Log.error(err);
+        }
+    });
+
+    beforeEach(function () {
+        Log.test(`BeforeTest: ${this.currentTest.title}`);
+    });
+
+    after(function () {
+        Log.test(`After: ${this.test.parent.title}`);
+    });
+
+    afterEach(function () {
+        // This section resets the data directory (removing any cached data) and resets the InsightFacade instance
+        // This runs after each test, which should make each test independent from the previous one
+        Log.test(`AfterTest: ${this.currentTest.title}`);
+        try {
+            fs.removeSync(cacheDir);
+            fs.mkdirSync(cacheDir);
+            insightFacade = new InsightFacade();
+        } catch (err) {
+            Log.error(err);
+        }
+    });
+
+    // This is a unit test. You should create more like this!
+    it("Should add a valid dataset", function () {
+        const id: string = "courses";
+        const expected: string[] = [id];
+        const futureResult: Promise<string[]> = insightFacade.addDataset(id, datasets[id], InsightDatasetKind.Courses);
+        return expect(futureResult).to.eventually.deep.equal(expected);
+    });
+});
+
+/*
+ * This test suite dynamically generates tests from the JSON files in test/queries.
+ * You should not need to modify it; instead, add additional files to the queries directory.
+ * You can still make tests the normal way, this is just a convenient tool for a majority of queries.
+ */
+describe("InsightFacade PerformQuery", () => {
+    const datasetsToQuery: { [id: string]: {path: string, kind: InsightDatasetKind} } = {
+        courses: {path: "./test/data/courses.zip", kind: InsightDatasetKind.Courses},
+    };
+    let insightFacade: InsightFacade;
+    let testQueries: ITestQuery[] = [];
+
+    // Load all the test queries, and call addDataset on the insightFacade instance for all the datasets
+    before(function () {
+        Log.test(`Before: ${this.test.parent.title}`);
+
+        // Load the query JSON files under test/queries.
+        // Fail if there is a problem reading ANY query.
+        try {
+            testQueries = TestUtil.readTestQueries();
+        } catch (err) {
+            expect.fail("", "", `Failed to read one or more test queries. ${err}`);
+        }
+
+        // Load the datasets specified in datasetsToQuery and add them to InsightFacade.
+        // Will fail* if there is a problem reading ANY dataset.
+        const loadDatasetPromises: Array<Promise<string[]>> = [];
+        insightFacade = new InsightFacade();
+        for (const id of Object.keys(datasetsToQuery)) {
+            const ds = datasetsToQuery[id];
+            const data = fs.readFileSync(ds.path).toString("base64");
+            loadDatasetPromises.push(insightFacade.addDataset(id, data, ds.kind));
+        }
+        return Promise.all(loadDatasetPromises).catch((err) => {
+            /* *IMPORTANT NOTE: This catch is to let this run even without the implemented addDataset,
+             * for the purposes of seeing all your tests run.
+             * TODO For C1, remove this catch block (but keep the Promise.all)
+             */
+            return Promise.resolve("HACK TO LET QUERIES RUN");
+        });
+    });
+
+    beforeEach(function () {
+        Log.test(`BeforeTest: ${this.currentTest.title}`);
+    });
+
+    after(function () {
+        Log.test(`After: ${this.test.parent.title}`);
+    });
+
+    afterEach(function () {
+        Log.test(`AfterTest: ${this.currentTest.title}`);
+    });
+
+    // Dynamically create and run a test for each query in testQueries
+    // Creates an extra "test" called "Should run test queries" as a byproduct. Don't worry about it
+    it("Should run test queries", function () {
+        describe("Dynamic InsightFacade PerformQuery tests", function () {
+            for (const test of testQueries) {
+                it(`[${test.filename}] ${test.title}`, function () {
+                    const futureResult: Promise<any[]> = insightFacade.performQuery(test.query);
+                    return TestUtil.verifyQueryResult(futureResult, test);
+                });
+            }
+        });
+    });
+});
