e665262acd01adb9fb90f606b8ff146060af76bb
kevchin <kevchin@student.ubc.ca>
2021-02-03 02:57:36 -0800

    added types for Sections and Datasets

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -6,6 +6,35 @@ import {
 } from "./IInsightFacade";
 import { InsightError, NotFoundError } from "./IInsightFacade";
 
+// Represents a dataset
+export interface Dataset {
+    // dataset id so we can pick this dataset when querying
+    id: string;
+    // array including all sections within this dataset
+    sections: Section[];
+}
+
+// Represents a section read from dataset
+export interface Section {
+    Section: string;
+
+    // json key         s fields
+    Subject: string;    // dept
+    Course: string;     // id
+    Professor: string;  // instructor
+    Title: string;      // title
+    id: string;         // uuid
+    // JSON.parse is going to not set id properly because this id is a number in JSON while the type here is string
+    // Prob needs to be set manually after doing JSON.parse
+
+    // json key         m fields
+    Avg: number;        // avg
+    Pass: number;       // pass
+    Fail: number;       // fail
+    Audit: number;      // audit
+    Year: number;       // year
+}
+
 /**
  * This is the main programmatic entry point for the project.
  * Method documentation is in IInsightFacade
@@ -15,6 +44,9 @@ export default class InsightFacade implements IInsightFacade {
     constructor() {
         Log.trace("InsightFacadeImpl::init()");
     }
+
+    // All datasets kept track of by this class
+    public datasets: Dataset[];
 
     public addDataset(
         id: string,
