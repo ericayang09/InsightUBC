5344640d5b092ea99e2ce0347a15bbcc3a9f1227
kevchin <kevchin@student.ubc.ca>
2021-02-12 02:12:24 -0800

    perform query returns sections that match query, but not sorted yet

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -7,6 +7,7 @@ import {
 import { InsightError, NotFoundError } from "./IInsightFacade";
 
 import { validateQuery } from "../QueryValidateLibrary";
+import { performQueryAfterValidation } from "../QueryPerformLibrary";
 
 // Represents a dataset
 export interface Dataset {
@@ -61,7 +62,6 @@ export default class InsightFacade implements IInsightFacade {
     }
 
     public performQuery(query: any): Promise<any[]> {
-
         /*
         let obj be a new Section with uninitialized values.
         then initialize values of obj that are requested by the Query (e.g. what's in COLUMNS).
@@ -77,13 +77,37 @@ export default class InsightFacade implements IInsightFacade {
             - what if two items are tied? e.g. order by alphabetical but both values are "CPSC". what order then?
         * */
 
+        this.datasets = [];
+        // need fake dataset + sections to test
+        let temp1: Section = { Section : "", Subject : "", Course : "201", Professor : "Baniassad, Elisa",
+            Title: "feafea", id: "100",
+            Avg: 91, Pass: 80, Fail: 10, Audit: 10, Year: 10 };
+        let temp2: Section = { Section : "", Subject : "", Course : "410", Professor : "Gregor Kiczales",
+            Title: "feafea", id: "101",
+            Avg: 80, Pass: 50, Fail: 10, Audit: 10, Year: 10 };
+        let temp3: Section = { Section : "", Subject : "", Course : "600", Professor : "Something",
+            Title: "feafea", id: "102",
+            Avg: 60, Pass: 20, Fail: 10, Audit: 10, Year: 10 };
+        let temp4: Section = { Section : "", Subject : "math", Course : "201", Professor : "feafpeao",
+            Title: "afea", id: "103",
+            Avg: 99.78, Pass: 10, Fail: 10, Audit: 10, Year: 10 };
+        let temp5: Section = { Section : "", Subject : "math", Course : "300", Professor : "feafafa",
+            Title: "faefa", id: "104",
+            Avg: 99.78, Pass: 10, Fail: 10, Audit: 10, Year: 10 };
+        let temp6: Section = { Section : "", Subject : "cnps", Course : "400", Professor : "feafeafa",
+            Title: "afeafa", id: "105",
+            Avg: 99.19, Pass: 10, Fail: 10, Audit: 10, Year: 10 };
+        let dataset: Dataset = { id: "courses", sections: [temp1, temp2, temp3, temp4, temp5, temp6]};
+        // add fake dataset
+        this.datasets.push(dataset);
+
         // Validate the Query. Checking for InsightError
         if (validateQuery(query) === false) {
             return Promise.reject(new InsightError());
         }
         // At this point, the query should be a perfectly valid query (except for NotFoundError and ResultTooLarge)
 
-        return Promise.reject("Not implemented.");
+        return performQueryAfterValidation(query, this.datasets); // Promise.reject("Not implemented.");
     }
 
     public listDatasets(): Promise<InsightDataset[]> {
